49ca43a5019610382375e447e2d63de89496d906
Eliot Quon <eliot.quon@nrel.gov>
2015-12-29 13:36:40 -0700

    Added some comments.

--- peaks.py
+++ peaks.py
@@ -9,6 +9,17 @@ if DEBUG: import matplotlib.pyplot as plt
 if TIMING: import time
 
 def find_peaks(data,x=[],Nsmoo=1):
+    """Find peaks in 2D data by checking for zeros and sign changes
+    in the slope. Slope is calculated with central differences and
+    the points with a local slope closest (or equal to) zero are
+    returned. 
+
+    A smoothing parameter selects the number of points to the left
+    and right to take in a moving average (default=1), prior to
+    slope calculation. 
+    """
+
+    # process inputs
 
     N = len(data)
     i = np.arange(N)
@@ -17,37 +28,48 @@ def find_peaks(data,x=[],Nsmoo=1):
     else:
         assert(len(x)==N)
     dx = np.diff(x)
-
-    if TIMING: t0 = time.time()
-    ist = i   - Nsmoo
-    ind = i+1 + Nsmoo
-    ist = np.maximum(0,ist)
-    ind = np.minimum(N,ind)
-    width = ind - ist
-    if TIMING: print ' - walltime [s] to calculate indices:',time.time()-t0
-
-    if TIMING: t0 = time.time()
     ysmoo = data.copy()
-    buff = np.zeros((N))
-    for ismoo in range(1,Nsmoo+1):
-        # add LHS
-        buff[:ismoo] = 0.
-        buff[ismoo:] = data[:N-ismoo]
-        if DEBUG: print -ismoo,buff
-        ysmoo += buff
-        # add RHS
-        buff[:N-ismoo] = data[ismoo:]
-        buff[N-ismoo:] = 0.
-        ysmoo += buff
-        if DEBUG: print ismoo,buff
-    ysmoo /= width
-    if TIMING: print ' - walltime [s] to smooth data:',time.time()-t0
+
+    if Nsmoo > 0:
+
+        # calculate start/end indices for smoothing
+
+        if TIMING: t0 = time.time()
+        ist = i   - Nsmoo
+        ind = i+1 + Nsmoo
+        ist = np.maximum(0,ist)
+        ind = np.minimum(N,ind)
+        width = ind - ist
+        if Nsmoo==0: assert( np.max(width)==1 )
+        if TIMING: print ' - walltime [s] to calculate indices:',time.time()-t0
+
+        # add additional values from left/right
+
+        if TIMING: t0 = time.time()
+        buff = np.zeros((N))
+        for ismoo in range(1,Nsmoo+1):
+            # add LHS
+            buff[:ismoo] = 0.
+            buff[ismoo:] = data[:N-ismoo]
+            if DEBUG: print -ismoo,buff
+            ysmoo += buff
+            # add RHS
+            buff[:N-ismoo] = data[ismoo:]
+            buff[N-ismoo:] = 0.
+            ysmoo += buff
+            if DEBUG: print ismoo,buff
+        ysmoo /= width
+        if TIMING: print ' - walltime [s] to smooth data:',time.time()-t0
+
+    # calculate slope from (smoothed) data
 
     if TIMING: t0 = time.time()
     twodx = dx[1:] + dx[:-1]
     dydx = np.zeros((N))
     dydx[1:-1] = (ysmoo[2:] - ysmoo[:-2]) / twodx
     if TIMING: print ' - walltime [s] to calculate slope:',time.time()-t0
+
+    # check for sign change in slope OR slope identically equal to 0
 
     if TIMING: t0 = time.time()
     signs = np.sign(dydx)
@@ -58,6 +80,8 @@ def find_peaks(data,x=[],Nsmoo=1):
     exact0s = np.nonzero( dydx==0 )[0]
     indices = np.unique( np.concatenate((idxchange,exact0s)) )
     if TIMING: print ' - walltime [s] to find peak indices:',time.time()-t0
+
+    # output stuff
 
     peaks = data[indices]
 
