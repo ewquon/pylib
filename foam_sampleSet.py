83b92100ebfb9cd71de745d616504265a591e7e1
Eliot Quon <eliot.quon@nrel.gov>
2016-08-05 17:03:21 -0600

    Updated foam_sampleSet (for line sampling)

--- foam_sampleSet.py
+++ foam_sampleSet.py
@@ -4,14 +4,15 @@ import numpy as np
 
 class uniform:
     """ For post-processing simple xyz-style output from uniform (line) set sample
-    Sets timeNames and sampleNames with the time directory and sample names, respectively.
-    Sets t to a numpy array of times
-    Sets fieldNames to the name of the scalar or vector fields
     """
 
     sampleExt = 'xy'
 
     def __init__(self,path='.'):
+        """ Sets timeNames and sampleNames with the time directory and sample names, respectively.
+        Sets t to a numpy array of times
+        Sets fieldNames to the name of the scalar or vector fields
+        """
         self.path = path
         self.t = []
         self.timeNames = []
@@ -37,11 +38,18 @@ class uniform:
             print 'No time directories found in', path
             return
 
+        # sort based on times
         order = np.argsort(self.t)
         self.t = self.t[order]
         self.timeNames = [ timeNames[i] for i in order ]
-        self.timeNames = [ timeNames[i] for i in order ]
+        dt = np.diff(self.t)
+        if np.max(dt)-np.min(dt) > 1e-14:
+            print 'Warning: gaps detected in sampling times'
+            self.dt = dt
+        else:
+            self.dt = dt[0]
 
+        # gather sample names
         path0 = os.path.join(path,timeNames[0])
         flist = [ f for f in os.listdir(path0) if os.path.isfile(os.path.join(path0, f)) ]
         for f in flist:
@@ -57,11 +65,11 @@ class uniform:
             s += '\n  %s' % name
         return s
 
-    def getSample(self,name,field):
-        """ Returns a sampled field over with specified name
-        Assumes x is identical for all samples
+    def getSample(self,name,field,verbose=True):
+        """ Returns a sampled field with specified name
+        Assumes 'x' is identical for all samples
         For a scalar field, the output array has shape (N,NX), where NX is the number of spatial samples and N is the length of the time array;
-        For a vector field, the output array has shape (N,NX,3)
+        for a vector field, the output array has shape (N,NX,3)
         """
         found = False
         suffix = '_' + field
@@ -75,51 +83,84 @@ class uniform:
 
         fname = f + '.' + self.sampleExt
 
-        # get position of field
-        pos = -(len(f) - f.index(suffix))/2
-    
-        # get number of lines (i.e., number of points in sample) and sampling locations
-        testfile = os.path.join(self.path, self.timeNames[0], fname)
-        with open(testfile, 'r') as f:
-            for i,_ in enumerate(f): pass
-        self.NX = i+1
+        xfile = self.path + os.sep + f + '.x.npy'
+        ufile = self.path + os.sep + f + '.'+field + '.npy'
+        try:
+            # read from pre-processed numpy data file
+            #x,U = np.load( savefile )
+            x = np.load( xfile )
+            U = np.load( ufile )
+            self.NX = U.shape[1]
+            print 'Data read from',ufile
 
-        print 'Found set in',fname,': field',field,'at position',pos,'with',self.NX,'samples'
+        except IOError:
 
-        x = np.zeros(self.NX)
-        with open(testfile, 'r') as f:
-            for i,line in enumerate(f):
-                x[i] = float( line.split()[0] )
-
-        # allocate
-        if field=='U': 
-            isVector = True
-            U = np.zeros( (self.N, self.NX, 3) )
-        else: 
-            isVector = False
-            U = np.zeros( (self.N, self.NX) )
+            # get position of field
+            pos = -(len(f) - f.index(suffix))/2
         
-        # process files in all time dirs
-        for it,tdir in enumerate(self.timeNames):
-            sys.stdout.write('\r  reading t= %f' % self.t[it])
-            with open(os.path.join(self.path, tdir, fname), 'r') as f:
-                if isVector:
-                    for i,line in enumerate(f):
-                        #Ux[it,i] = float( line.split()[3*pos+0] )
-                        #Uy[it,i] = float( line.split()[3*pos+1] )
-                        #Uz[it,i] = float( line.split()[3*pos+2] )
-                        U[it,i,:] = [ float(val) for val in line.split()[3*pos:][:3] ]
-                else:
-                    for i,line in enumerate(f):
-                        U[it,i] = float( line.split()[pos] )
-        print ''
+            # get number of lines (i.e., number of points in sample) and sampling locations
+            testfile = os.path.join(self.path, self.timeNames[0], fname)
+            with open(testfile, 'r') as f:
+                for i,_ in enumerate(f): pass
+            self.NX = i+1
+
+            print 'Found set in',fname,': field',field,'at position',pos,'with',self.NX,'samples'
+
+            x = np.zeros(self.NX)
+            with open(testfile, 'r') as f:
+                for i,line in enumerate(f):
+                    x[i] = float( line.split()[0] )
+
+            # allocate
+            if field=='U': 
+                isVector = True
+                U = np.zeros( (self.N, self.NX, 3) )
+            else: 
+                isVector = False
+                U = np.zeros( (self.N, self.NX) )
+            
+            # process files in all time dirs
+            for it,tdir in enumerate(self.timeNames):
+                sys.stdout.write('\r  reading t= %f' % self.t[it])
+                with open(os.path.join(self.path, tdir, fname), 'r') as f:
+                    if isVector:
+                        for i,line in enumerate(f):
+                            #Ux[it,i] = float( line.split()[3*pos+0] )
+                            #Uy[it,i] = float( line.split()[3*pos+1] )
+                            #Uz[it,i] = float( line.split()[3*pos+2] )
+                            U[it,i,:] = [ float(val) for val in line.split()[3*pos:][:3] ]
+                    else:
+                        for i,line in enumerate(f):
+                            U[it,i] = float( line.split()[pos] )
+            print ''
+
+            print '  saving',ufile
+            np.save( xfile, x )
+            np.save( ufile, U )
 
         # done reading sample
-        if isVector:
-            print field+'x [',np.min(U[:,:,0]),np.max(U[:,:,0]),']'
-            print field+'y [',np.min(U[:,:,1]),np.max(U[:,:,1]),']'
-            print field+'z [',np.min(U[:,:,2]),np.max(U[:,:,2]),']'
-        else:
-            print field+' [',np.min(U[:,:]),np.max(U[:,:]),']'
+        if verbose:
+            print '  x min/max : [',np.min(x),np.max(x),']'
+            if len(U.shape)==3:
+                #if isVector:
+                print ' ',field+'x min/max : [',np.min(U[:,:,0]),np.max(U[:,:,0]),']'
+                print ' ',field+'y min/max : [',np.min(U[:,:,1]),np.max(U[:,:,1]),']'
+                print ' ',field+'z min/max : [',np.min(U[:,:,2]),np.max(U[:,:,2]),']'
+            else:
+                print ' ',field+' min/max : [',np.min(U[:,:]),np.max(U[:,:]),']'
+
         return x,U
+
+
+#===============================================================================
+#===============================================================================
+#===============================================================================
+# test run
+if __name__=='__main__':
+
+    line0 = uniform(path='linesTransverse')
+    print line0
+
+    x,U = line0.getSample('line_08km','U')
+
 
