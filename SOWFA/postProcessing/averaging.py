c5266ab864673a5a72ea97d74efa1097c0538c45
Eliot Quon <eliot.quon@nrel.gov>
2016-09-20 16:50:12 -0600

    Created basic python version of SOWFA-Tools' ABLTools
    Currently includes functionality of ABLTools/variances_avg_cell.m
    
    Can run averaging.py from within the averaging dir for testing

---
+++ SOWFA/postProcessing/averaging.py
@@ -0,0 +1,260 @@
+#!/usr/bin/python
+"""
+For processing SOWFA cell averages in postProcessing/averaging
+Based on original SOWFA-Tools in MATLAB
+
+Sample usage:
+
+    from SOWFA.postProcessing.averaging import read
+
+    # read all time directories in current working directory
+    averagingData = read()
+
+    # read '0' and '1000' in current working directory
+    averagingData = read( 0, 1000 )
+
+    # read all time directories in specified directory
+    averagingData = read('caseX/postProcessing/averaging')
+
+    # read specified time directories
+    averagingData = read('caseX/postProcessing/averaging/0',
+                        'caseX/postProcessing/averaging/1000')
+
+"""
+import os
+import numpy as np
+
+def read(*args):
+    return averagingData(*args)
+
+class averagingData(object):
+
+    def __init__(self,*args):
+        """ Find and process all time directories
+        """# {{{
+        self.hLevelsCell = None
+        self.processed = False
+        self.simTimeDirs = [] # output time names
+        self.simStartTimes = [] # start or restart simulation times
+
+        # find results
+        for opt in args:
+            if not os.path.isdir(opt): continue
+            listing = os.listdir(opt)
+            if 'hLevelsCell' in listing:
+                # directly specified an output (time) directory
+                self.simTimeDirs.append( opt )
+                try:
+                    self.simStartTimes.append( float(opt) )
+                except: # specified results dir is not a number
+                    self.simStartTimes.append( -1 )
+            else:
+                # specified a directory containing output (time) subdirectories
+                for dirname in listing:
+                    if not os.path.isdir(dirname): continue
+                    try:
+                        startTime = float(dirname)
+                        if 'hLevelsCell' in os.listdir(dirname):
+                            self.simTimeDirs.append( dirname )
+                            self.simStartTimes.append( startTime )
+                    except ValueError: pass # dirname is not a number
+
+        # sort results
+        self.simTimeDirs = [ x[1] for x in sorted(zip(self.simStartTimes,self.simTimeDirs)) ]
+        self.simStartTimes.sort()
+
+        # process all output dirs
+        for idir,tdir in enumerate( self.simTimeDirs ):
+            print 'Processing',tdir
+            self._process(tdir)
+    # }}}
+
+    def _process(self,tdir):
+        """ Reads all files within an averaging output time directory, presumably containing hLevelsCell and other cell-averaged quantities
+        An object attribute corresponding to the averaged output name is updated; e.g., ${timeDir}/U_mean is appended to the array self.U_mean
+        """# {{{
+        self.processed = True
+
+        allOutputs = os.listdir(tdir)
+        outputs = []
+        for out in allOutputs:
+            if out=='hLevelsCell': continue
+            field = out[:-5] # strip '_mean' suffix
+            if len(field)==1 or field.startswith('R') or \
+                    (len(field)==2 and not field.startswith('T') and not field.startswith('q')):
+                outputs.append( out )
+
+        # process hLevelsCell first, verify we have the same cells
+        with open(tdir+os.sep+'hLevelsCell','r') as f:
+            line = f.readline()
+        z = np.array([ float(val) for val in line.split() ])
+        if getattr(self,'hLevelsCell') is None: # this is the first time dir
+            self.hLevelsCell = z
+        elif not np.all( self.hLevelsCell == z ):
+            print 'Error: cell levels do not match'
+            return
+
+        # check that we have the same amount of data
+        Nlines = []
+        for qty in outputs:
+            output = tdir + os.sep + qty
+            if not os.path.isfile(output): continue
+
+            with open(output,'r') as f:
+                for i,line in enumerate(f): pass
+                Nlines.append(i+1)
+                line = line.split() # check final line for the right number of values
+                if not len(line) == len(z)+2: # t,dt,f_1,f_2,...,f_N for N heights
+                    print 'z',z
+                    print 'line',line
+                    print 'Error: number of output points inconsistent with hLevelsCell in',output
+                    return
+
+        if not np.min(Nlines) == np.max(Nlines):
+            print 'Error: number of output times do not match in all files'
+            return
+        N = Nlines[0]
+
+        # now process all data
+        for qty in outputs:
+            output = tdir + os.sep + qty
+            if not os.path.isfile(output): continue
+
+            with open(output,'r') as f:
+                lines = f.readlines()
+            newdata = np.array([ [ float(val) for val in line.split()] for line in lines ]) # newdata.shape = (Nt,Nz+2)
+
+            try:
+                olddata = getattr(self,qty)
+                setattr( self, qty, np.concatenate((olddata,newdata[:,2:])) )
+                print '  concatenated',qty
+            except AttributeError:
+                setattr( self, qty, newdata[:,2:] )
+                print '  created',qty
+
+        # add additional times
+        try:
+            self.t = np.concatenate((self.t, newdata[:,0]))
+            self.dt = np.concatenate((self.dt, newdata[:,1]))
+        except AttributeError: # this is the first time dir
+            self.t = np.array( newdata[:,0] )
+            self.dt = np.array( newdata[:,1] )
+    # }}}
+
+    def TI(self,heights=[],avg_time=None,avg_width=300,SFS=False):
+        """ Calculate the turbulence intensity (TI) of the resolved fluctuations alone or combined fluctuations (resolved and sub-filter scale, SFS)
+        Based on ABLTools/variances_avg_cell.m
+
+        INPUTS
+            avg_time        time at which to calculate TI [s]
+            avg_width       number of samples over which to perform the statistics
+            heights         vertical locations at which to calculate TI [m]
+            SFS             set to True to include SFS terms
+
+        OUTPUTS
+            TIx             variance in x-dir
+            TIy             variance in y-dir
+            TIz             variance in z-dir
+            TIdir           variance resolved to flow direction
+            TIxyz           variance assuming homogeneous turbulence, calculated from TKE
+            TKE             turbulent kinetic energy (TKE)
+        """# {{{
+        Nout  = len(heights)
+        if Nout==0:
+            print 'Need to specify output heights'
+            return
+        TIx   = np.zeros(Nout)
+        TIy   = np.zeros(Nout)
+        TIz   = np.zeros(Nout)
+        TIdir = np.zeros(Nout)
+        TIxyz = np.zeros(Nout)
+        TKE   = np.zeros(Nout)
+        if not self.processed:
+            print 'No time directories were processed'
+            return TIx,TIy,TIz,TIdir,TIxyz,TKE
+
+        if avg_time is None:
+            avg_time = self.t[-1]
+        print 'Average at time',avg_time
+
+        dtmin = np.min(self.dt)
+        dtmax = np.max(self.dt)
+        if dtmin==dtmax:
+            print 'Constant dt, averaging window :',avg_width*dtmin,'s'
+        else:
+            dtmean = np.mean(self.dt)
+            print 'Variable dt, approximate averaging window :',avg_width*dtmean,'s'
+            print '  dt min/mean/max=',dtmin,dtmean,dtmax
+
+        i = np.argmin(np.abs(self.t-avg_time))
+        js = max( i-avg_width  , 0 )
+        je = min( i+avg_width+1, len(self.t) )
+        dtsub = self.dt[js:je]
+        print 'Processing range js,je,sum(dt):',js,je,np.sum(dtsub)
+
+        # calculate time-averaged velocity profiles
+        UMeanAvg = np.dot( dtsub, self.U_mean[js:je,:] ) / np.sum(dtsub)
+        VMeanAvg = np.dot( dtsub, self.V_mean[js:je,:] ) / np.sum(dtsub)
+        WMeanAvg = np.dot( dtsub, self.W_mean[js:je,:] ) / np.sum(dtsub)
+
+        # calculate time-averaged variances
+        uuMeanAvg = np.dot( dtsub, self.uu_mean[js:je,:] ) / np.sum(dtsub)
+        vvMeanAvg = np.dot( dtsub, self.vv_mean[js:je,:] ) / np.sum(dtsub)
+        uvMeanAvg = np.dot( dtsub, self.uv_mean[js:je,:] ) / np.sum(dtsub)
+        wwMeanAvg = np.dot( dtsub, self.ww_mean[js:je,:] ) / np.sum(dtsub)
+        if SFS:
+            print 'Adding SFS component'
+            uuMeanAvg += np.dot( dtsub, self.R11_mean[js:je,:] ) / np.sum(dtsub)
+            vvMeanAvg += np.dot( dtsub, self.R22_mean[js:je,:] ) / np.sum(dtsub)
+            uvMeanAvg += np.dot( dtsub, self.R12_mean[js:je,:] ) / np.sum(dtsub)
+            wwMeanAvg += np.dot( dtsub, self.R33_mean[js:je,:] ) / np.sum(dtsub)
+
+        # interpolate values to input heights
+        Ux = np.interp( heights, self.hLevelsCell, UMeanAvg )
+        Uy = np.interp( heights, self.hLevelsCell, VMeanAvg )
+        Uz = np.interp( heights, self.hLevelsCell, WMeanAvg )
+        sqrt_uuMeanAvg = np.interp( heights, self.hLevelsCell, np.sqrt(uuMeanAvg) ) # for agreement with variances_avg_cell.m
+        sqrt_uvMeanAvg = np.interp( heights, self.hLevelsCell, np.sqrt(uvMeanAvg) )
+        sqrt_vvMeanAvg = np.interp( heights, self.hLevelsCell, np.sqrt(vvMeanAvg) )
+        sqrt_wwMeanAvg = np.interp( heights, self.hLevelsCell, np.sqrt(wwMeanAvg) )
+        uuMeanAvg = np.interp( heights, self.hLevelsCell, uuMeanAvg )
+        uvMeanAvg = np.interp( heights, self.hLevelsCell, uvMeanAvg )
+        vvMeanAvg = np.interp( heights, self.hLevelsCell, vvMeanAvg )
+        wwMeanAvg = np.interp( heights, self.hLevelsCell, wwMeanAvg )
+
+        Umag = np.sqrt( Ux**2 + Uy**2 + Uz**2 )
+        print 'Umag at z=',heights,'m : ',Umag,'m/s'
+
+        # calculate wind direction
+        windDir = np.abs( np.arctan2(Uy,Ux) )
+
+        # calculate TKE and TI
+        TKE = 0.5*( uuMeanAvg + vvMeanAvg + wwMeanAvg )
+        TIx = sqrt_uuMeanAvg / Umag #np.sqrt( uuMeanAvg ) / Umag
+        TIy = sqrt_vvMeanAvg / Umag #np.sqrt( vvMeanAvg ) / Umag
+        TIz = sqrt_wwMeanAvg / Umag #np.sqrt( wwMeanAvg ) / Umag
+        TIxyz = np.sqrt( 2./3.*TKE ) / Umag
+
+        TIdir = uuMeanAvg *   np.cos(windDir)**2 \
+              + uvMeanAvg * 2*np.sin(windDir)*np.cos(windDir) \
+              + vvMeanAvg *   np.sin(windDir)**2
+        TIdir = np.sqrt(TIdir) / Umag
+
+        # }}}
+        return TIx,TIy,TIz,TIdir,TIxyz,TKE
+
+#===========================================================
+if __name__ == '__main__':
+    import glob
+
+    subdirs = glob.glob('*')
+    data = read(*subdirs)
+
+    TIx,TIy,TIz,TIdir,TIxyz,TKE = data.TI(avg_time=15000.,heights=[90.],SFS=True)
+    print 'TIx=',TIx
+    print 'TIy=',TIy
+    print 'TIz=',TIz
+    print 'TIdir=',TIdir
+    print 'TIxyz=',TIxyz
+    print 'TKE=',TKE
+
