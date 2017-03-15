1e5f7b023d339946085386b29825fddbc55e1b78
Eliot Quon <eliot.quon@nrel.gov>
2017-03-15 01:04:49 -0600

    Added VTK to VTP batch conversion script

---
+++ bin/vtk2vtp.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# based on https://gist.github.com/thomasballinger/1281457
+import sys
+import os
+
+import vtk
+
+def vtk2vtp(inputFile, outputFile):
+    """Convert legacy VTK file to new VTK Polygonal Data format
+
+    Note: binary files written on one system may not be readable on
+    another.
+    """
+    if not os.path.isfile(inputFile):
+        raise IOError('Input file not found: '+inputFile)
+
+    reader = vtk.vtkPolyDataReader()
+    reader.SetFileName(inputFile)
+    reader.Update()
+
+    writer = vtk.vtkXMLPolyDataWriter()
+    writer.SetFileName(outputFile)
+    #writer.SetFileTypeToBinary()
+    writer.SetInputData(reader.GetOutput())
+    writer.Update()
+
+#=======================================================================
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print 'specify file(s) to batch convert'
+        sys.exit()
+
+    for vtkfile in sys.argv[1:]:
+        if not vtkfile[-4:] == '.vtk':
+            print 'Skipping',vtkfile
+            continue
+
+        vtk2vtp(vtkfile, vtkfile[:-4]+'.vtp')
+
