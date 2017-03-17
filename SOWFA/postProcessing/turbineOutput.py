ef66b857c51e49a9c77dc6fc2bb7759586937b1c
Eliot Quon <eliot.quon@nrel.gov>
2017-03-17 17:15:07 -0600

    Added functionality to handle blade point data. Should probably convert to using xarray and convert bladePointX/Y/Z to additional indices

--- SOWFA/postProcessing/turbineOutput.py
+++ SOWFA/postProcessing/turbineOutput.py
@@ -23,20 +23,21 @@ def _processTurbineOutputHeader(line):
     headerNames.append(' '.join(line[i+1:]))
     return headerNames
 
-def readRotorOutputs(datadir='turbineOutput',Nturb=1,turbineList=None):
+def readRotorOutputs(datadir='turbineOutput',prefix='rotor',Nturb=1,turbineList=None):
     """Returns a dictionary of pandas dataframes for each turbine
 
     If turbineList is specified, then selected turbines are returned; 
     otherwise, all turbines (presumably Nturb many) are returned.
     """
     dataseries = TimeSeries(datadir,verbose=False)
-    rotorDataNames = dataseries.outputs('rotor')
+    #dataNames = dataseries.outputs(prefix)
+    dataNames = dataseries.outputs(prefix) + ['bladePitch']
     turbinedata = dict()
 
     if turbineList is None:
         turbineList = range(Nturb)
 
-    for dataname in rotorDataNames:
+    for dataname in dataNames:
         print 'Processing',dataname
         dataseries.setFilename(dataname)
         
@@ -54,7 +55,68 @@ def readRotorOutputs(datadir='turbineOutput',Nturb=1,turbineList=None):
             
             for iturb,turbNum in enumerate(turbineList):
                 next_df = next_df.loc[next_df['Turbine'] == turbNum]
-                # TODO: fix header names for data files with blade points
+                if irest==0:
+                    dframes.append(next_df)
+                    assert(len(dframes) == iturb+1)
+                else:
+                    dframes[iturb] = next_df.combine_first(dframes[iturb]) # if overlap, overwrite with next_df values
+        
+        # append full time series (with all restarts) to the complete turbinedata frame for each turbine
+        for iturb,turbNum in enumerate(turbineList):
+            if turbNum not in turbinedata.keys():
+                turbinedata[turbNum] = dframes[iturb]
+            else:
+                turbinedata[turbNum] = pd.concat(
+                        (turbinedata[turbNum], dframes[iturb].iloc[:,len(headerNames)-2:]),
+                        axis=1)
+
+    # sort everything by the (time) index once
+    for turbNum in turbineList:
+        turbinedata[turbNum].sort_index(inplace=True)
+
+    return turbinedata
+
+def readBladeOutputs(datadir='turbineOutput',prefix='blade',Nturb=1,bladeNum=0,turbineList=None):
+    """Returns a dictionary of pandas dataframes for each turbine
+
+    If turbineList is specified, then selected turbines are returned; 
+    otherwise, all turbines (presumably Nturb many) are returned.
+
+    For reference: http://pandas.pydata.org/pandas-docs/stable/reshaping.html
+    """
+    dataseries = TimeSeries(datadir,verbose=False)
+    dataNames = dataseries.outputs(prefix)
+    dataNames.remove('bladePitch') # this is the collective pitch for all blades, and should be a 'rotor' quantity
+    turbinedata = dict()
+
+    if turbineList is None:
+        turbineList = range(Nturb)
+
+    for dataname in dataNames:
+        print 'Processing',dataname
+        dataseries.setFilename(dataname)
+        
+        dframes = []
+        
+        # loop over restarts
+        for irest,fname in enumerate(dataseries):
+            print '  datafile',irest,':',fname
+            with open(fname,'r') as f:
+                headerNames = _processTurbineOutputHeader(f.readline())
+                testline = f.readline().split()
+                numBladePoints = len(testline) - len(headerNames) + 1
+                print '  (detected',numBladePoints,'blade points)'
+                fieldname = headerNames[-1]
+                headerNames = headerNames[:-1] + [ fieldname+'_'+str(ipt) for ipt in range(numBladePoints) ]
+            with open(fname,'r') as f:
+                f.readline() # skip header
+                next_df = pd.read_csv(f,
+                                      delim_whitespace=True,
+                                      header=None, names=headerNames, 
+                                     ).set_index('Time(s)') # Note: setting the index removes the time column
+            
+            for iturb,turbNum in enumerate(turbineList):
+                next_df = next_df.loc[(next_df['Turbine'] == turbNum) & (next_df['Blade'] == bladeNum)]
                 if irest==0:
                     dframes.append(next_df)
                     assert(len(dframes) == iturb+1)
