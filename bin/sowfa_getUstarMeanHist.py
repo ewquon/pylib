a35e5c46dd77218306ddafebb26c6fad29d6e9b4
Eliot Quon <eliot.quon@nrel.gov>
2018-03-04 10:58:07 -0700

    Updated uStarMean extraction script to handle multiple log files simultaneously

--- bin/sowfa_getUstarMeanHist.py
+++ bin/sowfa_getUstarMeanHist.py
@@ -5,39 +5,41 @@ import numpy as np
 
 histfile = 'uStarMeanHist.dat'
 
-
 makeplots = False
+logfiles = []
 for arg in sys.argv[1:]:
     if arg.lower().strip() == '-plot':
         import matplotlib.pyplot as plt
         makeplots = True
     else:
-        logfile = arg
+        logfiles.append(arg)
+if len(logfiles) == 0:
+    sys.exit('USAGE: '+sys.argv[0]+' logfile(s)')
 
 nsteps = 0
 startTime = -1
 simTimes = []
 uStarMean = []
-try:
-    with open(logfile,'r') as f:
-        for line in f:
-            if line.startswith('Create mesh'):
-                startTime = float(line.split()[-1])
-                if startTime > 0: print 'Detected restart from t =',startTime
-            elif line.startswith('Time ='):
-                curTime = float(line.split()[2])
-                simTimes.append(curTime)
-                nsteps += 1
-            elif line.startswith('uStarMean ='):
-                us = float(line.split()[2])
-                try:
-                    uStarMean[nsteps-1] = us
-                except IndexError:
-                    uStarMean.append(us)
-except NameError:
-    sys.exit('USAGE: '+sys.argv[0]+' log_file')
-except IOError:
-    sys.exit('Problem reading '+logfile)
+for logfile in logfiles:
+    print 'Processing',logfile
+    try:
+        with open(logfile,'r') as f:
+            for line in f:
+                if line.startswith('Create mesh'):
+                    startTime = float(line.split()[-1])
+                    if startTime > 0: print 'Detected restart from t =',startTime
+                elif line.startswith('Time ='):
+                    curTime = float(line.split()[2])
+                    simTimes.append(curTime)
+                    nsteps += 1
+                elif line.startswith('uStarMean ='):
+                    us = float(line.split()[2])
+                    try:
+                        uStarMean[nsteps-1] = us
+                    except IndexError:
+                        uStarMean.append(us)
+    except IOError:
+        sys.exit('Problem reading '+logfile)
 
 if os.path.isfile(histfile):
     np.savetxt(open(histfile,'a'),np.vstack((simTimes,uStarMean)).T,fmt='%g',delimiter='\t')
