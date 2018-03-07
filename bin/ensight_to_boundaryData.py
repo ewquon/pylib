a1dbb163db9f335c460ef9bbd0be45b470b58735
Eliot Quon <eliot.quon@nrel.gov>
2018-03-07 11:59:48 -0700

    Added utility to convert SOWFA sampled arrays of data in ensight format into timeVaryingMappedFixedValue boundaryData

---
+++ bin/ensight_to_boundaryData.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#
+# Converts SOWFA array sampling in ensight format to boundaryData.
+# Can be useful if downscaling is desired, e.g., sampling from a 20-m
+# precursor to be used as inflow to a 10-m domain. Also writes out a
+# compressed npz file with all of the boundary data.
+#
+# Note: This has only been tested for flow from the west.
+#
+# Written by Eliot Quon (eliot.quon@nrel.gov) -- 2018-03-07
+#
+import sys, os
+import numpy as np
+#from SOWFA.include import read_setUp
+from NWTC.datatools.timeseries import TimeSeries
+import NWTC.datatools.ensight as ens
+import NWTC.datatools.SOWFA.timeVaryingMappedBC as bc
+
+if len(sys.argv) <= 5:
+    sys.exit('USAGE: '+sys.argv[0]+' ensight_array_outputdir BC_name flow_dir plane_x ncells_y ncells_z [new_boundaryData_dir]')
+dpath = sys.argv[1]
+bcname = sys.argv[2]
+xinflow = sys.argv[3]
+ny = int(sys.argv[4])
+nz = int(sys.argv[5])
+if len(sys.argv) > 6:
+    boundaryData = sys.argv[6]
+else:
+    boundaryData = 'boundaryData'
+assert(bcname == 'west')  # only handle flow from west for now
+Ncells = ny * nz
+Npoints = (ny+1) * (nz+1)
+
+if os.path.isdir(boundaryData):
+    print('Warning:',boundaryData,'already exists!')
+
+#config = read_setUp()
+#yscale = ny / config['ny']
+#zscale = nz / config['nz']
+#print('Scaling factors:',yscale,zscale)
+
+ts = TimeSeries(dpath)
+print(ts)
+
+for time,dpath in zip(ts.outputTimes, ts.dirList):
+    tname = os.path.split(dpath)[-1]
+    print('t=',time)
+    pointsfile = os.path.join(boundaryData, bcname, 'points')
+    datapath = os.path.join(boundaryData, bcname, tname)
+    if not os.path.isfile(pointsfile):
+        os.makedirs(os.path.join(boundaryData, bcname))
+        # read mesh
+        # y = y.reshape((ny,nz), order='F')
+        # z = z.reshape((ny,nz), order='F')
+        x,y,z = ens.read_mesh(os.path.join(dpath, bcname+'_U.mesh'))
+        assert(len(x) == len(y) == len(z) == Ncells)
+        assert(np.min(x) == np.max(x)) # constant
+        x[:] = xinflow
+        bc.write_points(pointsfile, x, y, z, patchName=bcname)
+        print('  wrote',pointsfile)
+    if not os.path.isdir(datapath):
+        os.makedirs(datapath)
+        # process U
+        # u = u.reshape((ny,nz), order='F')
+        u,v,w = ens.read_vector(os.path.join(dpath, bcname+'_U.000.U'), Ncells)
+        bc.write_data(os.path.join(boundaryData, bcname, tname, 'U'),
+                      np.stack((u,v,w)), patchName=bcname)
+        # process T
+        T = ens.read_scalar(os.path.join(dpath, bcname+'_T_k.000.T'), Ncells)
+        bc.write_data(os.path.join(boundaryData, bcname, tname, 'T'),
+                      T, patchName=bcname)
+        # process k
+        k = ens.read_scalar(os.path.join(dpath, bcname+'_T_k.000.k'), Ncells)
+        bc.write_data(os.path.join(boundaryData, bcname, tname, 'k'),
+                      k, patchName=bcname)
+        print('  wrote data in',datapath)
+    else:
+        print('  did NOT overwrite data in',datapath)
+
