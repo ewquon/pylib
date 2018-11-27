d60ca87996983996150881ef565fc94b654ec64f
Eliot Quon <eliot.quon@nrel.gov>
2018-11-26 17:04:59 -0700

    Clean up screen output
    add indents
    remove area output (constant, and a large number which can be alarming)

--- bin/sowfa_watch.sh
+++ bin/sowfa_watch.sh
@@ -2,4 +2,7 @@
 logfile=`ls log.*Solver | tail -n 1`
 echo "Following $logfile"
 echo 'vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv'
-tail -f $logfile | grep -e '^Time =' -e '^Courant' -e '-Local Cell' -e 'maximum:' -e '-Boundary Flux' -e 'total - flux'
+tail -f $logfile \
+    | grep --line-buffered -e '^Time =' -e '^Courant' -e '-Local Cell' -e 'maximum:' -e '-Boundary Flux' -e 'total - flux' \
+    | sed -u 's/^Courant/   -Courant/g' \
+    | sed 's/\/ area.*$//g'
