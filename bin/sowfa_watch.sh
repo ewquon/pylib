e445bc213e9148ffd0f2a11a129d24adca18df23
Eliot Quon <eliot.quon@nrel.gov>
2018-12-17 18:04:37 -0700

    Watch SOWFA log for dictionary re-read

--- bin/sowfa_watch.sh
+++ bin/sowfa_watch.sh
@@ -6,6 +6,6 @@ logfile=`ls -t log.*$solver | head -n 1`
 echo "Following $logfile"
 echo 'vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv'
 tail -f $logfile \
-    | grep --line-buffered -e '^Time =' -e '^Courant' -e '-Local Cell' -e 'maximum:' -e '-Boundary Flux' -e 'total - flux' \
+    | grep --line-buffered -e '^Time =' -e '^Courant' -e '-Local Cell' -e 'maximum:' -e '-Boundary Flux' -e 'total - flux'  -e 'No Iterations 0' -e 'Re-reading object' \
     | sed -u 's/^Courant/   -Courant/g' \
     | sed 's/\/ area.*$//g'
